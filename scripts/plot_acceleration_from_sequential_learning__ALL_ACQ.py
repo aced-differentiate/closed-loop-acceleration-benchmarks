d1a00d4be8c9a0a6a13b1297a0458988b1a0092e
Vinay Hegde <vhegde@citrine.io>
2023-04-25 09:30:49 -0700

    y-label accuracy -> error

--- scripts/plot_acceleration_from_sequential_learning__ALL_ACQ.py
+++ scripts/plot_acceleration_from_sequential_learning__ALL_ACQ.py
@@ -483,7 +483,7 @@ def accuracy_per_iteration_plot(
     ax.set_ylim([0.025, max(mean) * 1.10])
     ax.set_xticks(np.arange(init_train_size, ax.get_xlim()[-1] + 1, 20))
     ax.set_xlabel("Size of training data")
-    ax.set_ylabel("Model test accuracy (MAE in eV)")
+    ax.set_ylabel("Model test error (MAE in eV)")
     print("Done.\n")
 
 
